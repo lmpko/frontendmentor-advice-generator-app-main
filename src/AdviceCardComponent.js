f8597f6e6185a1e6d1775550968f3264ec1e97a5
lmpko <lmpkoo@gmail.com>
2023-05-19 14:53:15 +0200

    fetch api, and get data after click on button

--- src/AdviceCardComponent.js
+++ src/AdviceCardComponent.js
@@ -1,19 +1,43 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import divide from "/images/pattern-divider-mobile.svg"
 import dice from "/images/icon-dice.svg"
 
 const AdviceCardComponent = () => {
+  const [data, setData] = useState([]);
+  const [isClicked, setIsClicked] = useState(false)
+
+  const handleClick = () => {
+    setIsClicked(true)
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.adviceslip.com/advice');
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+    setIsClicked(false)
+    return () => {
+      
+    };
+  }, [isClicked]);
+
   return (
-    
-    <div className='card'>
-      <span className='adviceID'>Advice #</span>
+    <div className='card'> 
+      <span className='adviceID'>Advice #{data?.slip?.id}</span>
       <blockquote>
-        "dsdfdsfsdfsdfs sdf dsf sdf df dsf dsf sfd"
+        "{data?.slip?.advice}"
       </blockquote>
 
       <img src={divide} alt="divide" className='divide-img' />
 
-      <button type="">
+      <button type="" onClick={handleClick}>
         <img src={dice} alt="dice"/>
       </button>
     </div>
